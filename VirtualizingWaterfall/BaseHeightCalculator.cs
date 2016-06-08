ceee2633aef752ef925fca9a24f7f34ddb4f54f1
ranbi <ranbi@RANBI-DESKTOP>
2016-06-08 21:21:05 +0800

    Add BaseHeightCalculator.

---
+++ VirtualizingWaterfall/BaseHeightCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualizingWaterfall
+{
+    public abstract class BaseHeightCalculator
+    {
+        public abstract double GetHeight(object data, double width);
+    }
+}
---
+++ VirtualizingWaterfall/SampleHeightCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualizingWaterfall
+{
+    public class SampleHeightCalculator : BaseHeightCalculator
+    {
+        public override double GetHeight(object data, double width)
+        {
+            SampleData sData = data as SampleData;
+            if (sData == null)
+                return 0;
+            else
+                return sData.FixedRenderHeight; 
+        }
+    }
+}
